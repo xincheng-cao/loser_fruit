1f175aa78d9a621c48895e2bfc0daecf77f0b9fd
xincheng-cao <36287551+xincheng-cao@users.noreply.github.com>
2023-06-05 13:44:19 +0800

    update

--- SortedContainers/855. Exam Room.py
+++ SortedContainers/855. Exam Room.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
+from sortedcontainers import SortedList
 
-class ExamRoom:
+
+class ExamRoom_outoftime:
 
     def __init__(self, n: int):
         self._seat=OrderedDict()
@@ -65,6 +67,79 @@ class ExamRoom:
     def leave(self, p: int) -> None:
         self._seat[p]=0
 
+
+class ExamRoom:
+
+    def __init__(self, n: int):
+        #self._seat=OrderedDict()
+        self._seat_len=n
+        self._sorted_list=SortedList()
+        #for i in range(n):
+        #    self._seat[i]=0
+
+
+    def seat(self) -> int:
+        seat_list=[]
+        for k in self._sorted_list:
+            seat_list.append(k)
+        #print(seat_list)
+
+        if len(seat_list)==0:
+            #self._seat[0]=1
+            self._sorted_list.add(0)
+            return 0
+        if len(seat_list)==1:
+            if seat_list[0]==0:
+                #self._seat[self._seat_len-1]=1
+                self._sorted_list.add(self._seat_len-1)
+                return self._seat_len-1
+            elif seat_list[0]==self._seat_len-1:
+                #self._seat[0]=1
+                self._sorted_list.add(0)
+                return 0
+            else:
+                if seat_list[0]==self._seat_len-1-seat_list[0]:
+                    #self._seat[0] = 1
+                    self._sorted_list.add(0)
+                    return 0
+                elif seat_list[0]>self._seat_len-1-seat_list[0]:
+                    #self._seat[0] = 1
+                    self._sorted_list.add(0)
+                    return 0
+                else:
+                    #self._seat[self._seat_len - 1] = 1
+                    self._sorted_list.add(self._seat_len - 1)
+                    return self._seat_len-1
+        #max_len=float('-inf')
+        #selected_seat=-1
+        if seat_list[0]>0:
+            selected_seat=0
+            max_len=seat_list[0]
+        else:
+            max_len = float('-inf')
+            selected_seat = None
+
+        for i in range(len(seat_list)-1):
+            if (seat_list[i+1]-seat_list[i])//2>max_len:
+                max_len=(seat_list[i+1]-seat_list[i])//2
+                selected_seat=max_len+seat_list[i]
+
+        if seat_list[-1]<self._seat_len-1:
+            if self._seat_len-1-seat_list[-1]>max_len:
+                max_len=self._seat_len-1-seat_list[-1]
+                selected_seat=self._seat_len-1
+
+        #self._seat[selected_seat]=1
+        self._sorted_list.add(selected_seat)
+
+
+
+        return selected_seat
+
+
+    def leave(self, p: int) -> None:
+        #self._seat[p]=0
+        self._sorted_list.remove(p)
 
 
 # Your ExamRoom object will be instantiated and called as such:
